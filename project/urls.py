a78b594227d3ff4ac890230ea59c02e71190872a
Miguel <jmrgc@cesar.school>
2026-09-02 09:32:37 -0300

    criação de hmtl e css

--- project/urls.py
+++ project/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',include("hello.urls")),
     path('forum/',include("forum.urls")),
-    path('', RedirectView.as_views(url='/forum', permanent=False), name='home'),
+    path('', RedirectView.as_view(url='/forum', permanent=False), name='home'),
+    
 ]
